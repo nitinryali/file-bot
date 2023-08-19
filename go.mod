dd171554c38e9493cce291640ed6802a6a8e6491
nitinryali <85013482+nitinryali@users.noreply.github.com>
2023-08-19 15:57:58 +0530

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/nitinryali/file-bot
+
+go 1.21.0
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/slack-go/slack v0.12.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-5764479105125-5764494116853-44TxeLOcXVingK1FvMCQatmw")
+	os.Setenv("CHANNEL_ID", "C05NMQKS67N")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"resume_plane.pdf"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL:%s\n", file.Name, file.URL)
+	}
+}
